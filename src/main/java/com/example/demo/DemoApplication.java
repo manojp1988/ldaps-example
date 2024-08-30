d41baa968c83c5b7218347f7f36cb2ec32206c6f
Manoj Periathambi <manoj@fastfacts.co>
2024-08-30 13:11:48 +0530

    LDAPS Sample

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import javax.naming.Context;
 import javax.naming.NamingEnumeration;
 import javax.naming.directory.*;
+import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
@@ -22,42 +24,34 @@ public class DemoApplication {
     @Bean
     ApplicationRunner runner() {
         return args -> {
-            final String ldapAdServer = "ldap://localhost:389";
 
-
-            final String ldapUsername = "uid=user.0,ou=People,dc=example,dc=com";
-            final String ldapPassword = "Newgen@123";
-
-            Hashtable<String, Object> env = new Hashtable<>();
-            env.put(Context.SECURITY_AUTHENTICATION, "simple");
-            env.put(Context.SECURITY_PRINCIPAL, ldapUsername);
-            env.put(Context.SECURITY_CREDENTIALS, ldapPassword);
-            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-            env.put(Context.PROVIDER_URL, ldapAdServer);
-            env.put("java.naming.ldap.attributes.binary", "objectSID");
-            DirContext ctx = new InitialDirContext(env);
-
-            String[] attrIDs = {"cn"};
-            SearchControls searchControls = new SearchControls();
-            searchControls.setReturningAttributes(attrIDs);
-            searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-
-            String filter = "(objectClass=person)";
-
-            NamingEnumeration<SearchResult> searchResults = ctx.search("dc=example,dc=com", filter, searchControls);
-
-            String commonName = null;
-            String distinguishedName = null;
-            while (searchResults.hasMore()) {
-
-                SearchResult result = (SearchResult) searchResults.next();
-                Attributes attrs = result.getAttributes();
-
-                distinguishedName = result.getNameInNamespace();
-                commonName = attrs.get("cn").toString();
-                System.out.println("Distinguised name :" + distinguishedName);
-                System.out.println("Common name :" + commonName);
-
+            String[] attrIDs = { "cn" };
+            SearchControls ctls = new SearchControls();
+            ctls.setReturningAttributes(attrIDs);
+            ctls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            String filter ="(objectClass=person)";
+            Hashtable env = new Hashtable();
+            env.put(DirContext.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+            env.put(DirContext.PROVIDER_URL, "ldaps://localhost:636" );
+            env.put(DirContext.SECURITY_AUTHENTICATION, "simple");
+            env.put(DirContext.SECURITY_PRINCIPAL, "uid=user.0,ou=People,dc=example,dc=com");
+            env.put(DirContext.SECURITY_CREDENTIALS, "Newgen@123");
+            env.put("java.naming.ldap.factory.socket", "com.example.demo.MySSLSocketFactory");
+            DirContext dirContext = null;
+            try {
+                dirContext  = new InitialDirContext(env);
+                NamingEnumeration e = dirContext.search("dc=example,dc=com", filter,ctls);
+                while (e.hasMore()) {
+                    SearchResult entry = (SearchResult) e.next();
+                    System.out.println(entry.getName());
+                    Attributes attrs = entry.getAttributes();
+                    Attribute attr = attrs.get("cn");
+                    var phone = (String) attr.get();
+                    System.out.println(phone);
+                }
+            }
+            catch (Exception e) {
+                System.err.println(e);
             }
 
             System.out.println("COmpleted..");
